231cbf77b5d8c04c39bb9fd64ea3b673bdeaa90b
Danny Vu <danieldukevu@outlook.com>
2021-01-22 10:28:57 +0800

    Initial commit - added styling

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [
+        { name: "Maximus", age: 28},
+        { name: "Manu", age: 29},
+        { name: "Stephanie", age: 26},
+      ]
+    }
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+        persons: [
+        { name: newName, age: 28},
+        { name: "Manu", age: 29},
+        { name: "Stephanie", age: 27},
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: "Maximus", age: 28},
+        { name: event.target.value, age: 29},
+        { name: "Stephanie", age: 26},
+      ]
+    } )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi! I'm a react app!</h1>
+        <button onClick={
+          () => this.switchNameHandler('Maximillian!!!')
+        }>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}  
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
